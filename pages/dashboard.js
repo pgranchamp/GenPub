1326f17a86968309ae4a2ae97bf1f8b8557aa1d4
pgranchamp <pierre.granchamp@finamars.com>
2025-04-30 13:30:57 +0200

    Update dashboard.js

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,1 +1,9 @@
 // Dashboard utilisateur : créer + afficher projets
+export default function Dashboard() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Tableau de bord</h1>
+      <p>Ici s'afficheront les projets de l'utilisateur.</p>
+    </div>
+  );
+}
