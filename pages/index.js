a2482498eebb3e9678ed1b1317554c880b301be4
pgranchamp <pierre.granchamp@finamars.com>
2025-04-30 13:31:22 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
 // Redirige vers /dashboard si logué
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/login");
+  }, []);
+  return null;
+}
